650dcb03c5361e957620692b036b0c893d03d8c2
Walter Nam <wkn2@case.edu>
2020-05-02 08:16:13 +0900

    Update reservation_system.sql

--- reservation_system.sql
+++ reservation_system.sql
@@ -13,10 +13,18 @@ CREATE TABLE Student(
     Student_ID INT,
     Student_name VARCHAR(20),
     Gender VARCHAR(20),
-    Preference VARCHAR(20),
     Expected_price INT,
     PRIMARY KEY(Student_ID)
 );
+
+CREATE TABLE Preference(
+    Student_ID INT,
+    Preference VARCHAR(20),
+    PRIMARY KEY(Student_ID, Preference),
+    foreign key(Student_ID)
+		references Student(Student_ID)
+);
+
 
 
 CREATE TABLE Reservation(
@@ -27,29 +35,82 @@ CREATE TABLE Reservation(
     PRIMARY KEY(Reservation_id)
 );
 
-CREATE TABLE Room(
-    Room_number INT,
-    Room_type VARCHAR(20),
-    Status VARCHAR(20),
-    Price INT,
-    PRIMARY KEY(Room_number)
-);
-
 CREATE TABLE Hotel(
     Hotel_ID INT,
     Hotel_name VARCHAR(20),
     Location VARCHAR(20),
     Rating INT,
-    Contact_info VARCHAR(20),
     PRIMARY KEY(Hotel_ID)
+);
+
+CREATE TABLE HotelContact(
+    Hotel_ID INT,
+    Contact_info VARCHAR(20),
+    PRIMARY KEY(Hotel_ID, Contact_info),
+    foreign key(Hotel_ID)
+		references Hotel(Hotel_ID)
 );
 
 CREATE TABLE Hotel_staff(
     Staff_ID INT,
     Staff_Name VARCHAR(20),
     Gender VARCHAR(20),
-    Language VARCHAR(20),
     PRIMARY KEY(Staff_ID)
+);
+
+CREATE TABLE StaffLanguage(
+    Staff_ID INT,
+    Languages VARCHAR(20),
+    PRIMARY KEY(Staff_ID, Languages),
+    foreign key(Staff_ID)
+		references Hotel_staff(Staff_ID)
+);
+
+The weak entity set Room and its identifying relationship set has_room are translated into a single table:
+
+CREATE TABLE has_room(
+    Room_number INT,
+    Room_type VARCHAR(20),
+    Status VARCHAR(20),
+    Price INT,
+    Hotel_ID INT,
+    PRIMARY KEY(Room_number, Hotel_ID),
+	FOREIGN KEY(Hotel_ID)
+		references Hotel(Hotel_ID)
+		on delete cascade
+);
+
+CREATE TABLE has_reservation(
+    Student_ID INT,
+    Reservation_ID INT,
+    PRIMARY KEY(Student_ID, Reservation_ID),
+    FOREIGN KEY(Student_ID)
+		references Student(Student_ID),
+	FOREIGN KEY(Reservation_ID)
+		references Reservation(Reservation_ID)
+);
+
+CREATE TABLE has_staff(
+    Staff_ID INT,
+    Hotel_ID INT,
+    PRIMARY KEY(Staff_ID, Hotel_ID),
+    FOREIGN KEY(Staff_ID)
+		references Hotel_staff(Staff_ID),
+	FOREIGN KEY(Hotel_ID)
+		references Hotel(Hotel_ID)
+);
+
+CREATE TABLE assign_room(
+    Reservation_ID INT,
+    Hotel_ID INT,
+    Room_number INT,
+    PRIMARY KEY(Room_number, Hotel_ID, Reservation_ID),
+	FOREIGN KEY(Hotel_ID)
+		references Hotel(Hotel_ID),
+	FOREIGN KEY(Reservation_ID)
+		references Reservation(Reservation_ID),
+	FOREIGN KEY(Hotel_ID,Room_number)
+		references has_room(Hotel_ID,Room_number)
 );
 
 
